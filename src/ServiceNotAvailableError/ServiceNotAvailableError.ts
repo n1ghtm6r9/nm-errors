783a3a5da666fa869e8b3ab7eadbf2a27c40d960
Bogdan Sklyarov <n1ghtm6r9@gmail.com>
2024-03-19 20:23:54 +0200

    fix(): bugs

--- src/ServiceNotAvailableError/ServiceNotAvailableError.ts
+++ src/ServiceNotAvailableError/ServiceNotAvailableError.ts
@@ -3,9 +3,6 @@ import { BaseError } from '../BaseError';
 export class ServiceNotAvailableError extends BaseError {
   public static readonly code = 'SERVICE_NOT_AVAILABLE_ERROR';
   constructor(serviceName: string, route: string) {
-    super({
-      code: ServiceNotAvailableError.code,
-      message: `Service "${serviceName}" route "${route}" not available.`,
-    });
+    super(`Service "${serviceName}" route "${route}" not available.`);
   }
 }
